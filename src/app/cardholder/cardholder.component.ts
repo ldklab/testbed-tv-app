888afc9d9ac5fcf3a7daac7fe835db7fb9ae75c4
Xfox1 <xfox1@hotmail.it>
2019-08-12 17:08:12 -0400

    Added few more cards

--- src/app/cardholder/cardholder.component.ts
+++ src/app/cardholder/cardholder.component.ts
@@ -13,18 +13,16 @@ export class CardholderComponent {
   loadingVideo = false;
   videoUrl = null;
 
-  constructor(private sanitizer: DomSanitizer) {}
-
-  cards = [
+  allCards = [
     {title: 'Cats', image: 'assets/img/thumbnails/thumb1.webp', video: 'hY7m5jjJ9mM'},
     {title: 'More cats', image: 'assets/img/thumbnails/thumb2.webp', video: 'XyNlqQId-nk'},
     {title: 'AC/DC - Back In Black', image: 'assets/img/thumbnails/thumb3.webp', video: '6vImyP5EYc8'},
     {title: 'Arctic Monkeys - Do I Wanna Know?', image: 'assets/img/thumbnails/thumb4.webp', video: 'bpOSxM0rNPM'},
     {title: 'Kygo, Chelsea Cutler - Not Ok', image: 'assets/img/thumbnails/thumb5.webp', video: 'MFdzgnMOmlw'},
-    {title: 'More', image: 'assets/img/thumbnails/thumb6.webp', video: 'XyNlqQId'},
-    {title: 'More', image: 'assets/img/thumbnails/thumb7.webp', video: 'XyNlqQId'},
-    {title: 'More', image: 'assets/img/thumbnails/thumb8.webp', video: 'XyNlqQId'},
-    {title: 'More', image: 'assets/img/thumbnails/thumb9.webp', video: 'XyNlqQId'},
+    {title: 'Avicii - Without You “Extended Mix” ft. Sandro Cavazza', image: 'assets/img/thumbnails/thumb6.webp', video: 'ZyYQQa2HlAI'},
+    {title: 'Ron Burgundy Targets Shawn Mendes in His Stand-Up Debut', image: 'assets/img/thumbnails/thumb7.webp', video: 'JRgoVGu4WHQ'},
+    {title: 'Bill Burr - Animation - Helicopter Bit', image: 'assets/img/thumbnails/thumb8.webp', video: 'S9ZSzuj1UpA'},
+    {title: 'Dave Chappelle Pulls Off An Impossible Punchline | Netflix Is A Joke', image: 'assets/img/thumbnails/thumb9.webp', video: 'VHkHw-NQ5Wk'},
     // {title: 'Chrome', image: 'assets/img/app-icons/chrome.png'},
     // {title: 'Drive', image: 'assets/img/app-icons/drive.png'},
     // {title: 'Earth', image: 'assets/img/app-icons/earth.png'},
@@ -35,6 +33,14 @@ export class CardholderComponent {
     // {title: 'Play Music', image: 'assets/img/app-icons/play-music.png'},
     // {title: 'Play Store', image: 'assets/img/app-icons/play-store.png'}
   ];
+
+  cards = [];
+
+  constructor(private sanitizer: DomSanitizer) {
+    // Select 9 random Cards
+    const shuffled = this.allCards.sort(() => 0.5 - Math.random());
+    this.cards = shuffled.slice(0, 9);
+  }
 
   playVideo(card) {
     console.log('PLAAAY: ' + card.video);
